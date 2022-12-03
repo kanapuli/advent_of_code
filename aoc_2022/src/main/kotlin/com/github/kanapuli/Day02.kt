6dbaaeb9c8eff9dc282c8a5e02507f53fd420db5
kanapuli <athavankanapuli@gmail.com>
2022-12-03 07:49:35 +0100

    Remove dictionaries and handle the winHand/lossHand combo inside Enums

--- aoc_2022/src/main/kotlin/com/github/kanapuli/Day02.kt
+++ aoc_2022/src/main/kotlin/com/github/kanapuli/Day02.kt
@@ -7,10 +7,10 @@ import java.io.File
  * GameInputs is the possible inputs in rock,scissors and paper game
  * and each member is defined with its score
  */
-enum class GameInputs(val score: Int) {
-    ROCK(1),
-    PAPER(2),
-    SCISSORS(3),
+enum class GameInputs(val score: Int, val winHand: String, val lossHand: String) {
+    ROCK(1, "Y", "Z"),
+    PAPER(2, "Z", "X"),
+    SCISSORS(3, "X", "Y"),
 }
 
 /**
@@ -23,18 +23,6 @@ enum class RoundResults(val score: Int, val strategy: String) {
 }
 
 fun main() {
-    val winningHand = mapOf<String, String>(
-        "A" to "Y",
-        "B" to "Z",
-        "C" to "X"
-    )
-
-    val losingHand = mapOf<String, String>(
-        "A" to "Z",
-        "B" to "X",
-        "C" to "Y"
-    )
-
     val hand = mapOf<String, GameInputs>(
         "A" to GameInputs.ROCK,
         "X" to GameInputs.ROCK,
@@ -46,11 +34,11 @@ fun main() {
 
     readFile("Day02.txt")
         .also {
-            val score = problem0201(it, winningHand, hand)
+            val score = problem0201(it, hand)
             println(score) // 12458
         }
         .also {
-            val score = problem0202(it, winningHand, losingHand, hand)
+            val score = problem0202(it, hand)
             println(score) // 12683
         }
 }
@@ -58,7 +46,7 @@ fun main() {
 /**
  * problem0201 calculates the score of a rock,paper,scissors game between two players
  */
-fun problem0201(file: File, winningHand: Map<String, String>, hand: Map<String, GameInputs>): Int {
+fun problem0201(file: File, hand: Map<String, GameInputs>): Int {
     return file.readLines().map {
         val (player1, player2) = it.split(" ")
         when {
@@ -66,7 +54,7 @@ fun problem0201(file: File, winningHand: Map<String, String>, hand: Map<String, 
                 hand[player2]!!.score + RoundResults.DRAW.score
             }
 
-            winningHand[player1] == player2 -> {
+            hand[player1]!!.winHand == player2 -> {
                 hand[player2]!!.score + RoundResults.WIN.score
             }
 
@@ -86,20 +74,18 @@ fun problem0201(file: File, winningHand: Map<String, String>, hand: Map<String, 
  */
 fun problem0202(
     file: File,
-    winningHand: Map<String, String>,
-    losingHand: Map<String, String>,
     hand: Map<String, GameInputs>
 ): Int {
     return file.readLines().map {
         val (player1, player2) = it.split(" ")
         when (player2) {
             RoundResults.WIN.strategy -> {
-                val strategyHand = winningHand[player1]
+                val strategyHand = hand[player1]!!.winHand
                 hand[strategyHand]!!.score + RoundResults.WIN.score
             }
 
             RoundResults.LOSS.strategy -> {
-                val strategyHand = losingHand[player1]
+                val strategyHand = hand[player1]!!.lossHand
                 hand[strategyHand]!!.score + RoundResults.LOSS.score
             }
 
