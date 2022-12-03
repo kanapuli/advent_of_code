2308fbd9ffd72b64063ca99b6fc77cb7fe1e7ea8
kanapuli <athavankanapuli@gmail.com>
2022-12-03 07:43:27 +0100

    Calculate score for rock,paper,scissors game based on the strategy guide

--- aoc_2022/src/main/kotlin/com/github/kanapuli/Day02.kt
+++ aoc_2022/src/main/kotlin/com/github/kanapuli/Day02.kt
@@ -14,12 +14,12 @@ enum class GameInputs(val score: Int) {
 }
 
 /**
- * ResultsScore is the combination of possible results in a game and its score
+ * RoundResults is the combination of possible results in a game and its score
  */
-enum class ResultsScore(val score: Int) {
-    WIN(6),
-    LOSS(0),
-    DRAW(3)
+enum class RoundResults(val score: Int, val strategy: String) {
+    WIN(6, "Z"),
+    LOSS(0, "X"),
+    DRAW(3, "Y")
 }
 
 fun main() {
@@ -28,6 +28,13 @@ fun main() {
         "B" to "Z",
         "C" to "X"
     )
+
+    val losingHand = mapOf<String, String>(
+        "A" to "Z",
+        "B" to "X",
+        "C" to "Y"
+    )
+
     val hand = mapOf<String, GameInputs>(
         "A" to GameInputs.ROCK,
         "X" to GameInputs.ROCK,
@@ -37,10 +44,15 @@ fun main() {
         "Z" to GameInputs.SCISSORS,
     )
 
-    readFile("Day02.txt").let {
-        val score = problem0201(it, winningHand, hand)
-        println(score) // 12458
-    }
+    readFile("Day02.txt")
+        .also {
+            val score = problem0201(it, winningHand, hand)
+            println(score) // 12458
+        }
+        .also {
+            val score = problem0202(it, winningHand, losingHand, hand)
+            println(score) // 12683
+        }
 }
 
 /**
@@ -51,19 +63,53 @@ fun problem0201(file: File, winningHand: Map<String, String>, hand: Map<String, 
         val (player1, player2) = it.split(" ")
         when {
             hand[player1] == hand[player2] -> {
-                hand[player2]!!.score + ResultsScore.DRAW.score
+                hand[player2]!!.score + RoundResults.DRAW.score
             }
 
             winningHand[player1] == player2 -> {
-                hand[player2]!!.score + ResultsScore.WIN.score
+                hand[player2]!!.score + RoundResults.WIN.score
             }
 
             else -> {
-                hand[player2]!!.score + ResultsScore.LOSS.score
+                hand[player2]!!.score + RoundResults.LOSS.score
             }
 
         }
     }.reduce { acc, x ->
         acc + x
+    }
+}
+
+/**
+ * problem0202 chooses the player 2 hand based on a strategy guide and calculates the score
+ * The strategy guide is defined in the RoundResults enum
+ */
+fun problem0202(
+    file: File,
+    winningHand: Map<String, String>,
+    losingHand: Map<String, String>,
+    hand: Map<String, GameInputs>
+): Int {
+    return file.readLines().map {
+        val (player1, player2) = it.split(" ")
+        when (player2) {
+            RoundResults.WIN.strategy -> {
+                val strategyHand = winningHand[player1]
+                hand[strategyHand]!!.score + RoundResults.WIN.score
+            }
+
+            RoundResults.LOSS.strategy -> {
+                val strategyHand = losingHand[player1]
+                hand[strategyHand]!!.score + RoundResults.LOSS.score
+            }
+
+            RoundResults.DRAW.strategy -> {
+                hand[player1]!!.score + RoundResults.DRAW.score
+            }
+
+            else -> 0
+        }
+    }.reduce { acc, i ->
+        acc + i
     }
 }
